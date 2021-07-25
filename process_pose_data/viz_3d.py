b4f3d6c4d651167345be99c450dcd93e0fcdadc5
Theodore Quinn <tcquinn@tcquinn.org>
2021-07-25 09:06:51 -0600

    Clean up comments

--- process_pose_data/viz_3d.py
+++ process_pose_data/viz_3d.py
@@ -15,7 +15,6 @@ def convert_all_data_to_json(
     poses_3d_with_person_info_df = poses_3d_with_person_info_df.copy()
     poses_3d_with_person_info_df.index.name = 'pose_3d_id'
     poses_3d_with_person_info_df.reset_index(inplace=True)
-    # poses_3d_with_person_info_df.sort_values('timestamp', inplace=True)
     poses_3d_with_person_info_df['keypoint_coordinates_3d'] = poses_3d_with_person_info_df['keypoint_coordinates_3d'].apply(lambda x: x.tolist())
     poses_3d_with_person_info_df = poses_3d_with_person_info_df.reindex(columns=[
         'timestamp',
@@ -28,13 +27,11 @@ def convert_all_data_to_json(
     ])
     # Prepare person position data
     person_positions = person_positions.copy()
-    # person_positions.sort_values('timestamp', inplace=True)
     person_positions['sensor_coordinates'] = person_positions['sensor_coordinates'].apply(lambda x: x.tolist())
     person_positions = person_positions.astype('object')
     person_positions = person_positions.where(pd.notnull(person_positions), None)
     # Prepare tray position data
     tray_positions = tray_positions.copy()
-    # tray_positions.sort_values('timestamp', inplace=True)
     tray_positions['sensor_coordinates'] = tray_positions['sensor_coordinates'].apply(lambda x: x.tolist())
     tray_positions = tray_positions.astype('object')
     tray_positions = tray_positions.where(pd.notnull(tray_positions), None)
@@ -44,9 +41,7 @@ def convert_all_data_to_json(
         set(person_positions['timestamp']) |
         set(tray_positions['timestamp'])
     ))
-    # poses_3d_with_person_info_df['timestamp'] = poses_3d_with_person_info_df['timestamp'].apply(lambda x: x.isoformat())
-    # person_positions['timestamp'] = person_positions['timestamp'].apply(lambda x: x.isoformat())
-    # tray_positions['timestamp'] = tray_positions['timestamp'].apply(lambda x: x.isoformat())
+    # Create consolidated data
     data_dict = OrderedDict()
     for timestamp in timestamps:
         data_dict[timestamp.isoformat()] = {
