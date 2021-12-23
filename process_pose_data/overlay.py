1b2659d704ef81162f0fc1631e1a668dd9176011
Theodore Quinn <tcquinn@tcquinn.org>
2021-12-23 13:06:02 -0800

    Move draw_pose_2d_opencv() to poseconnect library (attempt 1)

--- process_pose_data/overlay.py
+++ process_pose_data/overlay.py
@@ -415,7 +415,7 @@ def overlay_poses_timestamp(
                 pose_label = row[pose_label_column]
             else:
                 pose_label = None
-            image=draw_pose_2d_opencv(
+            image=poseconnect.overlay.draw_pose_2d_opencv(
                 image=image,
                 keypoint_coordinates=keypoint_coordinates_2d,
                 pose_label=pose_label,
@@ -529,7 +529,7 @@ def overlay_poses_camera_time_segment(
                 pose_label = row[pose_label_column]
             else:
                 pose_label = None
-            frame=draw_pose_2d_opencv(
+            frame=poseconnect.overlay.draw_pose_2d_opencv(
                 image=frame,
                 keypoint_coordinates=keypoint_coordinates_2d,
                 pose_label=pose_label,
@@ -771,7 +771,7 @@ def visualize_3d_pose_reconstruction(
         # Load image into memory
         image = cv.imread(image_metadata_dict.get(camera_id).get('image_local_path'))
         # Draw projected 3D pose in one color
-        image = draw_pose_2d_opencv(
+        image = poseconnect.overlay.draw_pose_2d_opencv(
             image=image,
             keypoint_coordinates=pose_3d_keypoint_coordinates_2d,
             draw_keypoint_connectors=draw_keypoint_connectors,
@@ -798,7 +798,7 @@ def visualize_3d_pose_reconstruction(
                 pose_2d_color = constituent_pose_2d_color
             else:
                 pose_2d_color = non_constituent_pose_2d_color
-            image = draw_pose_2d_opencv(
+            image = poseconnect.overlay.draw_pose_2d_opencv(
                 image=image,
                 keypoint_coordinates=pose_2d['keypoint_coordinates_2d'],
                 draw_keypoint_connectors=draw_keypoint_connectors,
@@ -971,7 +971,7 @@ def draw_poses_2d_timestamp_camera_opencv(
         background_image = cv_utils.fetch_image_from_local_drive(image_local_path)
     new_image = background_image
     for pose_2d_id, row in df.iterrows():
-        new_image = draw_pose_2d_opencv(
+        new_image = poseconnect.overlay.draw_pose_2d_opencv(
             image=new_image,
             keypoint_coordinates=row['keypoint_coordinates_2d'],
             draw_keypoint_connectors=draw_keypoint_connectors,
@@ -994,92 +994,92 @@ def draw_poses_2d_timestamp_camera_opencv(
         fig.set_size_inches(fig_width_inches, fig_height_inches)
     return new_image
 
-def draw_pose_2d_opencv(
-    image,
-    keypoint_coordinates,
-    draw_keypoint_connectors=True,
-    keypoint_connectors=None,
-    pose_label=None,
-    pose_color='green',
-    keypoint_radius=3,
-    keypoint_alpha=0.6,
-    keypoint_connector_alpha=0.6,
-    keypoint_connector_linewidth=3,
-    pose_label_color='white',
-    pose_label_background_alpha=0.6,
-    pose_label_font_scale=1.5,
-    pose_label_line_width=1
-):
-    pose_color = matplotlib.colors.to_hex(pose_color, keep_alpha=False)
-    pose_label_color = matplotlib.colors.to_hex(pose_label_color, keep_alpha=False)
-    keypoint_coordinates = np.asarray(keypoint_coordinates).reshape((-1, 2))
-    if not np.any(np.all(np.isfinite(keypoint_coordinates), axis=1), axis=0):
-        return image
-    valid_keypoints = np.all(np.isfinite(keypoint_coordinates), axis=1)
-    plottable_points = keypoint_coordinates[valid_keypoints]
-    new_image = image
-    for point_index in range(plottable_points.shape[0]):
-        new_image = cv_utils.draw_circle(
-            original_image=new_image,
-            coordinates=plottable_points[point_index],
-            radius=keypoint_radius,
-            line_width=1,
-            color=pose_color,
-            fill=True,
-            alpha=keypoint_alpha
-        )
-    if draw_keypoint_connectors and (keypoint_connectors is not None):
-        for keypoint_connector in keypoint_connectors:
-            keypoint_from_index = keypoint_connector[0]
-            keypoint_to_index = keypoint_connector[1]
-            if valid_keypoints[keypoint_from_index] and valid_keypoints[keypoint_to_index]:
-                new_image=cv_utils.draw_line(
-                    original_image=new_image,
-                    coordinates=[
-                        keypoint_coordinates[keypoint_from_index],
-                        keypoint_coordinates[keypoint_to_index]
-                    ],
-                    line_width=keypoint_connector_linewidth,
-                    color=pose_color,
-                    alpha=keypoint_connector_alpha
-                )
-    if pd.notna(pose_label):
-        pose_label_anchor = np.nanmean(keypoint_coordinates, axis=0)
-        text_box_size, baseline = cv.getTextSize(
-            text=str(pose_label),
-            fontFace=cv.FONT_HERSHEY_PLAIN,
-            fontScale=pose_label_font_scale,
-            thickness=pose_label_line_width
-        )
-        new_image=cv_utils.draw_rectangle(
-            original_image=new_image,
-            coordinates=[
-                [
-                    pose_label_anchor[0] - text_box_size[0]/2,
-                    pose_label_anchor[1] - (text_box_size[1] + baseline)/2
-                ],
-                [
-                    pose_label_anchor[0] + text_box_size[0]/2,
-                    pose_label_anchor[1] + (text_box_size[1] + baseline)/2
-                ]
-            ],
-            line_width=1.5,
-            color=pose_color,
-            fill=True,
-            alpha=pose_label_background_alpha
-        )
-        new_image=cv_utils.draw_text(
-            original_image=new_image,
-            coordinates=pose_label_anchor,
-            text=str(pose_label),
-            horizontal_alignment='center',
-            vertical_alignment='middle',
-            font_face=cv.FONT_HERSHEY_PLAIN,
-            font_scale=pose_label_font_scale,
-            line_width=pose_label_line_width,
-            color=pose_label_color
-        )
-    return new_image
+# def draw_pose_2d_opencv(
+#     image,
+#     keypoint_coordinates,
+#     draw_keypoint_connectors=True,
+#     keypoint_connectors=None,
+#     pose_label=None,
+#     pose_color='green',
+#     keypoint_radius=3,
+#     keypoint_alpha=0.6,
+#     keypoint_connector_alpha=0.6,
+#     keypoint_connector_linewidth=3,
+#     pose_label_color='white',
+#     pose_label_background_alpha=0.6,
+#     pose_label_font_scale=1.5,
+#     pose_label_line_width=1
+# ):
+#     pose_color = matplotlib.colors.to_hex(pose_color, keep_alpha=False)
+#     pose_label_color = matplotlib.colors.to_hex(pose_label_color, keep_alpha=False)
+#     keypoint_coordinates = np.asarray(keypoint_coordinates).reshape((-1, 2))
+#     if not np.any(np.all(np.isfinite(keypoint_coordinates), axis=1), axis=0):
+#         return image
+#     valid_keypoints = np.all(np.isfinite(keypoint_coordinates), axis=1)
+#     plottable_points = keypoint_coordinates[valid_keypoints]
+#     new_image = image
+#     for point_index in range(plottable_points.shape[0]):
+#         new_image = cv_utils.draw_circle(
+#             original_image=new_image,
+#             coordinates=plottable_points[point_index],
+#             radius=keypoint_radius,
+#             line_width=1,
+#             color=pose_color,
+#             fill=True,
+#             alpha=keypoint_alpha
+#         )
+#     if draw_keypoint_connectors and (keypoint_connectors is not None):
+#         for keypoint_connector in keypoint_connectors:
+#             keypoint_from_index = keypoint_connector[0]
+#             keypoint_to_index = keypoint_connector[1]
+#             if valid_keypoints[keypoint_from_index] and valid_keypoints[keypoint_to_index]:
+#                 new_image=cv_utils.draw_line(
+#                     original_image=new_image,
+#                     coordinates=[
+#                         keypoint_coordinates[keypoint_from_index],
+#                         keypoint_coordinates[keypoint_to_index]
+#                     ],
+#                     line_width=keypoint_connector_linewidth,
+#                     color=pose_color,
+#                     alpha=keypoint_connector_alpha
+#                 )
+#     if pd.notna(pose_label):
+#         pose_label_anchor = np.nanmean(keypoint_coordinates, axis=0)
+#         text_box_size, baseline = cv.getTextSize(
+#             text=str(pose_label),
+#             fontFace=cv.FONT_HERSHEY_PLAIN,
+#             fontScale=pose_label_font_scale,
+#             thickness=pose_label_line_width
+#         )
+#         new_image=cv_utils.draw_rectangle(
+#             original_image=new_image,
+#             coordinates=[
+#                 [
+#                     pose_label_anchor[0] - text_box_size[0]/2,
+#                     pose_label_anchor[1] - (text_box_size[1] + baseline)/2
+#                 ],
+#                 [
+#                     pose_label_anchor[0] + text_box_size[0]/2,
+#                     pose_label_anchor[1] + (text_box_size[1] + baseline)/2
+#                 ]
+#             ],
+#             line_width=1.5,
+#             color=pose_color,
+#             fill=True,
+#             alpha=pose_label_background_alpha
+#         )
+#         new_image=cv_utils.draw_text(
+#             original_image=new_image,
+#             coordinates=pose_label_anchor,
+#             text=str(pose_label),
+#             horizontal_alignment='center',
+#             vertical_alignment='middle',
+#             font_face=cv.FONT_HERSHEY_PLAIN,
+#             font_scale=pose_label_font_scale,
+#             line_width=pose_label_line_width,
+#             color=pose_label_color
+#         )
+#     return new_image
 
 def draw_polygon_2d_opencv(
     image,
@@ -1213,7 +1213,7 @@ def draw_poses_3d_timestamp_camera_opencv(
                     [camera_calibration['image_width'], camera_calibration['image_height']]
                 ]
             )
-            new_image=draw_pose_2d_opencv(
+            new_image=poseconnect.overlay.draw_pose_2d_opencv(
                 image=new_image,
                 keypoint_coordinates=keypoint_coordinates_2d,
                 draw_keypoint_connectors=draw_keypoint_connectors,
@@ -1409,7 +1409,7 @@ def draw_poses_2d_timestamp_camera(
             )
             keypoint_connectors = pose_model.get('keypoint_connectors')
     for pose_2d_id, row in df.iterrows():
-        draw_pose_2d_opencv(
+        poseconnect.overlay.draw_pose_2d_opencv(
             row['keypoint_coordinates_2d'],
             draw_keypoint_connectors=draw_keypoint_connectors,
             keypoint_connectors=keypoint_connectors,
